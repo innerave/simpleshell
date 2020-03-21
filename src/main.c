b3d1a20d19f88976af5c63f1492fd9a937ed2d0a
Perfilyev Vadim <vadimperfilev@gmail.com>
2020-03-21 23:05:37 +0300

    code clean up
    Signed-off-by: Perfilyev Vadim <vadimperfilev@gmail.com>

--- src/main.c
+++ src/main.c
@@ -5,55 +5,33 @@
 #include <termios.h>
 #include <unistd.h>
 #include <limits.h>
-<<<<<<< HEAD
-=======
 #include <errno.h>
 #include <sys/wait.h>
->>>>>>> code refactoring, string parsing
 
 pid_t child_pid;
 int stat_loc;
 
-<<<<<<< HEAD
-int read_line(char *line, size_t size);
-int parse_line(char *input, char **output, const int n);
-
-int main(){
-=======
 int parse_line(char*, char**, const int);
 int read_line(char*, size_t);
 
 int main()
 {
->>>>>>> code refactoring, string parsing
     char to_read[MAX_INPUT];
     char* parsed[50];
-    
+
     while(1){
         printf("simpleshell>");
         if (read_line(to_read, MAX_INPUT) == -1) return -2;
-<<<<<<< HEAD
-        if (parse_line(parsed,to_read, 50) == -1) return -3;
-
-=======
         if (parse_line(to_read, parsed, 50) == -1) return -3;
->>>>>>> code refactoring, string parsing
         child_pid = fork();
         if (child_pid == 0)
         {
             /* Never returns if the call is successful */
-<<<<<<< HEAD
-            printf("%s %s", to_read,  parsed[0]);
-            int r = execvp(parsed[0], parsed);
-            printf("This won't be printed if execvp is successul %d\n", r);
-        } else {
-=======
             execvp(parsed[0], parsed);
             printf("This won't be printed if execvp is successul\n errno=%d\n", errno);
         }
         else
         {
->>>>>>> code refactoring, string parsing
             waitpid(child_pid, &stat_loc, WUNTRACED);
         }
 
@@ -88,15 +66,9 @@ int read_line(char *line, size_t size){
     return -2;
 }
 
-<<<<<<< HEAD
-int parse_line(char *input, char **output, const int n) {
-    char *separator = " ";
-    char *parsed;
-=======
 int parse_line(char* input, char** output, const int n) {
     char* separator = " ";
     char* parsed;
->>>>>>> code refactoring, string parsing
     int index = 0;
 
     parsed = strtok(input, separator);
